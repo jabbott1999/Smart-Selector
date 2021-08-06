eb5b3f5046d3fbf90d10fe7219654298549dc779
James Abbott <jabbott@financialforce.com>
2021-08-06 15:09:37 +0100

    working with basic patterns'

--- src/extension.ts
+++ src/extension.ts
@@ -27,13 +27,18 @@ function smartSelect() {
 }
 
 function getNewIndexes(): {start: number, end: number} {
+  if (isInnerSelection()) {
+    console.log('Is Inner Selection');
+    return {start: editor.selection.start.character - 1, end: editor.selection.end.character + 1};
+  }
+
   const newStartIndex: number = getNewStartIndex();
   const newEndIndex: number = getIndexOfMatchingSymbol(symbolPairs.get(getCharacterByIndex(newStartIndex - 1)));
   return {start: newStartIndex, end: newEndIndex};
 }
 
 function getNewStartIndex(): number {
-  let startIndex: number = editor.selection.start.character;
+  let startIndex: number = isOuterSelection() ? editor.selection.start.character - 1: editor.selection.start.character;
 
   while (startIndex > -1) {
     if (symbolPairs.get(getCharacterByIndex(startIndex))) {
@@ -49,13 +54,33 @@ function getIndexOfMatchingSymbol(symbolToMatch: string): number {
   let endIndex: number = editor.selection.end.character;
 
   while (endIndex < editor.document.getText().length) {
-    if (getCharacterByIndex(endIndex) == symbolToMatch) {
+    if (getCharacterByIndex(endIndex) === symbolToMatch) {
       return endIndex;
     }
     endIndex ++;
   } 
 
 	return editor.document.getText().length;
+}
+
+function isInnerSelection(): Boolean {
+  const startCharacter: String = getCharacterByIndex(editor.selection.start.character - 1);
+  
+  if (symbolPairs.get(startCharacter) === getCharacterByIndex(editor.selection.end.character)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isOuterSelection(): Boolean {
+  const startCharacter: String = getCharacterByIndex(editor.selection.start.character);
+  
+  if (symbolPairs.get(startCharacter) === getCharacterByIndex(editor.selection.end.character - 1)) {
+    return true;
+  }
+
+  return false;
 }
 
 function select(selection: {start: number, end: number}): vscode.Selection {
@@ -66,5 +91,5 @@ function select(selection: {start: number, end: number}): vscode.Selection {
 }
 
 function getCharacterByIndex(index: number): string {
-  return editor.document.getText(new vscode.Selection(editor.document.positionAt(index), editor.document.positionAt(index + 1)))
+  return editor.document.getText(new vscode.Selection(editor.document.positionAt(index), editor.document.positionAt(index + 1)));
 }
